3805cdccae1a4c09b9cef3124c5dbbcd9b710918
DevClarkMiller <clarkmillermail@gmail.com>
2025-03-15 13:14:13 -0400

    Made the file upload part more clear on what it's doing

--- frontend/QrCool/src/components/ContentTypeForms.jsx
+++ frontend/QrCool/src/components/ContentTypeForms.jsx
@@ -1,4 +1,4 @@
-import { useContext, useRef } from "react";
+import { useContext, useRef, useMemo } from "react";
 
 // Components
 import LabeledInputField from "./LabeledInputField";
@@ -8,6 +8,9 @@ import FormButton from './FormButton';
 
 // Content
 import { ContentContext } from "../context/ContentProvider";
+
+// Icons
+import { IoCloudUploadSharp } from "react-icons/io5";
 
 const FormNameField = ({entryContentRed, entryContentController}) =>{
     return(
@@ -64,17 +67,10 @@ export const TextForm = () =>{
     );
 }
 
-function FileDropButton({onUploadClick}){
+function FileDropButton({onUploadClick, hasFile}){
     return(
-        <button type="button" onClick={onUploadClick} className="flex items-center justify-center w-3/4">
-            <label htmlFor="dropzone-file" className="col-flex-center nice-trans justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-                <div className="col-flex-center justify-center pt-5 pb-6">
-                    <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
-                    </svg>
-                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                </div>
-            </label>
+        <button type="button" onClick={onUploadClick} className={`justify-center ${hasFile ? "text-green-600" : "text-black"} text-4xl icon-blue-hover`}>
+            <IoCloudUploadSharp />
         </button> 
     );
 }
@@ -95,11 +91,13 @@ export const FileForm = () =>{
         fileInputRef.current.click();
     }
 
+    const hasFile = useMemo(() => file !== null, [file]);
+
     return(
         <ul className="size-full col-flex-center gap-3">
             <FormNameField entryContentRed={entryContentRed} entryContentController={entryContentController} />
+           
             <LabeledInputField
-    
                 className="text-regular font-bold text-left hidden"
                 inputField={<InputField 
                     ref={fileInputRef}
@@ -110,7 +108,10 @@ export const FileForm = () =>{
                     type="file"
                 />}
             >Upload File</LabeledInputField>
-            <FileDropButton onUploadClick={onUploadClick}/>
+            <div className="flex items-center justify-center gap-3">
+                <FileDropButton onUploadClick={onUploadClick} hasFile={hasFile}/>
+                {file && <div className="text-regular font-bold text-2xl">{file?.name}</div>}
+            </div>
         </ul>
     );
 }
