e1f0e9dbace2d0d3f97e78cfbacfd95c744486a5
Clark <clarkmillermail@gmail.com>
2025-09-11 15:24:15 -0400

    Updated unit tests to work without a hard-coded entryId

--- backend/src/test/report.test.ts
+++ backend/src/test/report.test.ts
@@ -3,18 +3,22 @@ import {describe, expect, test, it} from '@jest/globals';
 import ExcelViewReport from '../infrastructure/reporter/excelViewReporter';
 import EntryDao from '../dao/entryDao';
 import EntryViewDao from '../dao/entryViewDao';
-import { Entry, EntryView } from '@prisma/client';
+import { Account, Entry, EntryView } from '@prisma/client';
+import AccountDao from 'src/dao/accountDao';
 
 loadEnv();
 
 describe('Create ExcelViewReport', () => {
     // First get an entry
+    const accDao = new AccountDao();
     const entDao = new EntryDao();
     const evDao = new EntryViewDao();
+    let account: Account;
     let entry: Entry;
     let xlViewReport: ExcelViewReport;
     it("should construct", async () =>{
-        entry = await entDao.getById(1); // Just get the first entry
+        account = await accDao.getByUsername('Mbfrk');
+        entry = (await entDao.getByAccount(account.Id))[0]; // Just get the first entry
         const evs: EntryView[] = await evDao.getByEntry(entry.Id);
         xlViewReport = new ExcelViewReport(evs);
     });
