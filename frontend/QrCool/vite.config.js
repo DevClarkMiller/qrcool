6e138c9d2bb3720bee10d9cb9dba4db86b812294
Clark <clarkmillermail@gmail.com>
2025-09-15 19:44:47 -0400

    Updated vite.config to not ouput environment variables

--- frontend/QrCool/vite.config.js
+++ frontend/QrCool/vite.config.js
@@ -3,13 +3,9 @@ import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
 
 // Load environment variables from .env file
-// dotenv.config();
-
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   // Load env variables based on the mode (development or production)
   const env = loadEnv(mode, process.cwd(), '');
-  console.log(env);
   return {
     plugins: [react()],
     define: {
